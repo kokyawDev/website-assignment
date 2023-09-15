e9844af4967d4585265fb5d224c5b54b72221945
Ko Ko Kyaw <kokokyaw@myansis.com>
2023-09-16 01:12:30 +0630

    just commit

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Rules\ProductValidationRule;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $query = Order::query();
+
+        if(request()->has('keyword')) {
+            $query = $query->where('customer_name', 'like', '%'.request()->keyword.'%');
+        }
+
+        $orders = $query->latest()->paginate(5);
+
+        return view('admin.orders.index')
+            ->with([
+                'orders' => $orders,
+                'keyword' => request()->keyword ?? null
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+        abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        abort(404);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order $order)
+    {
+        abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Order $order)
+    {
+        return view('admin.orders.edit')
+            ->with([
+                'order' => $order
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order $order): \Illuminate\Http\RedirectResponse
+    {
+        $data = $request->validate([
+            'customer_name' => 'required|string|max:255',
+            'customer_email' => 'required|email|max:255',
+            'customer_phone' => 'required|string|max:255',
+            'payment_method' => 'required|string|max:255',
+            'shipping_method' => 'required|string|max:255',
+            'payment_status' => 'required|string|max:255',
+            'shipping_status' => 'required|string|max:255',
+            'shipping_address' => 'required|string|max:255',
+            'status' => 'required|string|max:255',
+            'order_note' => 'nullable|string|max:255',
+            'products' => ['required', new ProductValidationRule()],
+            'products.*.id' => 'required',
+            'products.*.qty' => 'required'
+        ]);
+
+        $this->updateOrder($data, $order);
+
+        return redirect()
+            ->route('admin.orders.index')
+            ->with('success', 'Order has been updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order $order)
+    {
+        $order->order_items()->delete();
+
+        $order->delete();
+
+        return redirect()->route('admin.orders.index')
+            ->with([
+                'success' => 'Your order has been deleted successfully.'
+            ]);
+    }
+
+    public function updateOrder($data, $order)
+    {
+        $order_items = collect($data['products']);
+
+        $order_data = Arr::except($data, 'products');
+
+        $sub_total = 0;
+
+        $order->order_items()->delete();
+
+        foreach ($order_items as $order_item) {
+            $product = Product::FindOrFail($order_item['id']);
+
+            $qty_price = ($product->discounted_price) ? $order_item['qty'] * $product->discounted_price : $order_item['qty'] * $product->price;
+
+            $sub_total += $qty_price;
+
+            OrderItem::create([
+                'product_id' => $product->id,
+                'quantity' => $order_item['qty'],
+                'price' => $qty_price,
+                'order_id' => $order->id
+            ]);
+        }
+
+        $order_data['total_price'] = $sub_total;
+
+        $order->update($order_data);
+
+        return $order->refresh();
+    }
+}
---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $query = Product::query();
+
+        if(request()->has('keyword')) {
+            $query = $query->where('name', 'like', '%'.request()->keyword.'%');
+        }
+
+        $products = $query->latest()->paginate(5);
+
+        return view('admin.products.index')
+            ->with([
+                'products' => $products,
+                'keyword' => request()->keyword ?? null
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('admin.products.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+            'description' => ['required', 'min:3', 'max:10000'],
+            'short_description' => ['required', 'min:3', 'max:255'],
+            'price' => ['required', 'numeric'],
+            'discounted_price' => ['nullable', 'numeric'],
+            'category_id' => ['required', 'exists:categories,id'],
+            'thumbnail' => ['required', 'image'],
+            'quantity' => ['nullable', 'numeric'],
+            'product_images' => ['required'],
+            'product_images.*' => ['required', 'image']
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            if($request->hasFile('thumbnail')) {
+                $file = $request->file('thumbnail');
+                $extension = $file->getClientOriginalExtension();
+                $filename = time() . '.' . $extension;
+                $file->move('uploads/products/', $filename);
+                $data['thumbnail'] = '/uploads/products/'.$filename;
+            }
+
+            $data['status'] = 'New';
+
+            $product = Product::create($data);
+
+            if($request->hasFile('product_images')){
+                foreach($request->file('product_images') as $file){
+                    $extension = $file->getClientOriginalExtension();
+                    $filename = time() . '.' . $extension;
+                    $file->move('uploads/products/', $filename);
+
+                    ProductImage::create([
+                        'product_id' => $product->id,
+                        'image' => '/uploads/products/'.$filename
+                    ]);
+                }
+            }
+
+            DB::commit();
+
+            return redirect()->route('admin.products.index')->with('success', 'Product has been created successfully!');
+
+        }  catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        $categories = Category::all();
+
+        return view('admin.products.edit')
+            ->with([
+                'categories' => $categories,
+                'product' => $product
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+            'description' => ['required', 'min:3', 'max:10000'],
+            'short_description' => ['required', 'min:3', 'max:255'],
+            'price' => ['required', 'numeric'],
+            'discounted_price' => ['nullable', 'numeric'],
+            'category_id' => ['required', 'exists:categories,id'],
+            'thumbnail' => ['nullable', 'image'],
+            'quantity' => ['nullable', 'numeric'],
+            'product_images' => ['nullable', 'image'],
+            'product_images.*' => ['nullable', 'image']
+        ]);
+
+        $product_data = Arr::except($data, 'product_images');
+
+        DB::beginTransaction();
+
+        try {
+            if($request->hasFile('thumbnail')) {
+                $file = $request->file('thumbnail');
+                $extension = $file->getClientOriginalExtension();
+                $filename = time() . '.' . $extension;
+                $file->move('uploads/products/', $filename);
+                $product_data['thumbnail'] = '/uploads/products/'.$filename;
+            }
+
+            $product = $product->update($product_data);
+
+            if($request->hasFile('product_images')){
+                $product->images()->delete();
+
+                foreach($request->file('product_images') as $file){
+                    $extension = $file->getClientOriginalExtension();
+                    $filename = time() . '.' . $extension;
+                    $file->move('uploads/products/', $filename);
+
+                    ProductImage::create([
+                        'product_id' => $product->id,
+                        'image' => '/uploads/products/'.$filename
+                    ]);
+                }
+            }
+
+            DB::commit();
+
+            return redirect()->route('admin.products.index')->with('success', 'Product has been updated successfully!');
+
+        }  catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->images()->delete();
+
+        $product->delete();
+
+        return redirect()->route('admin.products.index')->with('success', 'Product has been deleted successfully!');
+
+    }
+}
